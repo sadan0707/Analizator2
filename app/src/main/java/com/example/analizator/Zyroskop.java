28c99f95751664d3440528bade6ec3c5d6e1860c
sadan0707 <sadan0707@gmail.com>
2016-01-18 15:39:44 +0100

    -

---
+++ app/src/main/java/com/example/analizator/Zyroskop.java
@@ -0,0 +1,14 @@
+package com.example.analizator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by krzysiek on 2016-01-18.
+ */
+public class Zyroskop  extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zyroskop);
+    }
+}
